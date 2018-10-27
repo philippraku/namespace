bc6d81ec52c6f3fed85a58e3b9e418015fc5997f
philippraku <43067753+philippraku@users.noreply.github.com>
2018-10-26 22:19:29 -0400

    Add files via upload

---
+++ namespace.js
@@ -0,0 +1,25 @@
+var PHILIPPRAKU = {
+
+    init: function () {
+
+        let divA = document.createElement("div");
+        divA.className = "box";
+        divA.textContent = "PHILIPPRAKU";
+
+        let divAs = document.getElementById("boxes");
+        divAs.appendChild(divA);
+
+        divA.addEventListener("click", function () {
+            divA.style.borderColor = "#000000";
+            divA.style.backgroundColor = "#b3c6ff";
+            document.write("Phil The Thrill!")
+        });
+        divA.addEventListener("mouseover", function () {
+            divA.classList.toggle("highlight");
+        });
+        divA.addEventListener("mouseout", function () {
+            divA.classList.toggle("highlight");
+            divA.removeAttribute("style");
+        });
+    }
+};
